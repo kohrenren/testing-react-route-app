92e203527ecd564645dbf4d9273248128690a443
kohrenren <renwei97_@hotmail.com>
2025-04-03 01:25:42 +0800

    Sidebar fix

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,24 +1,60 @@
-import React, { useState } from "react";
-import { Link } from 'react-router-dom'; // Correct import
+import React, { useEffect, useRef, useState } from "react";
+import { Link, useLocation } from 'react-router-dom'; // Correct import
 import '../styles/global.css';
 
-const Sidebar = () => {
-  const [isOpen, setIsOpen] = useState(true); // Sidebar toggle state
+const navLinks = [
+  { path: "/home", label: "Home" },
+  { path: "/about", label: "About" },
+  { path: "/features", label: "Features" },
+];
 
+const Sidebar = () => {
+  // const [isOpen, setIsOpen] = useState(true); // Sidebar toggle state
+  const location = useLocation();
+  const [activeIndex, setActiveIndex] = useState(navLinks.findIndex(link => link.path === location.pathname) || 0);
+  const indicatorRef = useRef(null); // Reference to the indicator div
+  const tabRef = useRef([]); //References for each tab
+  const [imagesLoadedCount, setImagesLoadedCount] = useState(0); 
+
+  const handleImageLoad = () => {
+    setImagesLoadedCount((prev) => prev + 1); // Increment count on each image load
+  };
+
+  useEffect(() => {
+    if (imagesLoadedCount === 2) { // When both images are loaded
+      updateIndicator();
+    }
+  }, [imagesLoadedCount, activeIndex]);
+  
+
+  const updateIndicator = () => {
+    if (tabRef.current[activeIndex] && indicatorRef.current) {
+      const { offsetTop, offsetHeight } = tabRef.current[activeIndex];
+      indicatorRef.current.style.top = `${offsetTop}px`;
+      indicatorRef.current.style.height = `${offsetHeight}px`;
+    }
+  };
+  
   return (
     <div className=" pattern pattern--black sidebar">
-      <img src="/pokemon-scarlet-logo-medium-up.webp" alt="Pikachu Icon" />
-      <img src="/pokemon-violet-logo-medium-up.webp" alt="Pikachu Icon" />
+      {/* <div className="sideline"></div> */}
+      <img src="/pokemon-scarlet-logo-medium-up.webp" alt="Pikachu Icon" onLoad={handleImageLoad}/>
+      <img src="/pokemon-violet-logo-medium-up.webp" alt="Pikachu Icon" onLoad={handleImageLoad}/>
       <nav>
         <ul>
-          <li>
-            <Link to="/home">Home</Link> {/* Link to HomePage */}
+        {navLinks.map(({path, label}, index) => (
+          <li 
+            key={path}
+            ref={(el) => (tabRef.current[index] = el)}
+            className={`nav-item ${activeIndex === index ? "active" : ""}`}
+            onClick={() => setActiveIndex(index)}
+          >
+            <Link to={path}>{label}</Link> 
           </li>
-          <li>
-            <Link to="/about">About</Link> {/* Link to AboutPage */}
-          </li>
+          ))}
         </ul>
       </nav>
+      <div ref={indicatorRef} className="tab-indicator"></div>
     </div>
   );
 };
